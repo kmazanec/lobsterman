ef590e663a0d1fd27b9351025376e8f7305d6ed3
kmazanec <k.mazanec@gmail.com>
2013-08-15 11:41:11 -0400

    New Application created, initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lobsterman::Application.config.secret_key_base = '40b3c7eb64a110bc38a4bf934aca6aae56fbb6b6d98cf0b2ab3086075e544e4cb993c2911b62cacd353eedd1b19997c16992f7619c7681fcd1dbb3a387104e24'
